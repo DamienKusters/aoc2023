9acdc0a1d638716bba354e1d6d849d226905118b
Damien Kusters <damien.kusters@ziggo.nl>
2023-12-02 12:32:37 +0100

    day2 part2

--- day02/main.go
+++ day02/main.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	part1()
+	part2()
 }
 
 var blockLimits = map[string]int{
@@ -27,8 +28,7 @@ func part1() {
 	for _, d := range games {
 		game := strings.Split(d, ":")
 		if game[0] == "" {
-			//TODO: result
-			// fmt.Println(validGameIds)
+			sum = 0
 			for _, g := range validGameIds {
 				sum += g
 			}
@@ -59,6 +59,48 @@ func part1() {
 			gameid, _ := strconv.Atoi(strings.Split(game[0], " ")[1])
 			validGameIds = append(validGameIds, gameid)
 		}
+	}
+
+}
+
+var gameSolutions []int
+
+func part2() {
+	rawData, _ := os.ReadFile("input.txt")
+	games := strings.Split(strings.ReplaceAll(string(rawData), "\r\n", "\n"), "\n")
+
+	for _, d := range games {
+		game := strings.Split(d, ":")
+		if game[0] == "" {
+			sum = 0
+			for _, g := range gameSolutions {
+				sum += g
+			}
+			fmt.Print("Part2: ")
+			fmt.Println(sum)
+			return
+		}
+		rounds := strings.Split(game[1], ";")
+
+		lowestBlocks := map[string]int{
+			"red":   0,
+			"green": 0,
+			"blue":  0,
+		}
+
+		for _, r := range rounds {
+			blocks := strings.Split(r, ",")
+			for _, b := range blocks {
+				block := strings.Split(strings.TrimSpace(b), " ")
+				blockValue, _ := strconv.Atoi(block[0])
+				blockKind := block[1]
+
+				if blockValue > lowestBlocks[blockKind] {
+					lowestBlocks[blockKind] = blockValue
+				}
+			}
+		}
+		gameSolutions = append(gameSolutions, lowestBlocks["red"]*lowestBlocks["green"]*lowestBlocks["blue"])
 	}
 
 }
